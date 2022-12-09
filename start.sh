933be5ba3257093d3428580e2b15afc2820829a3
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2022-12-09 17:30:59 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/meharfaizan/Unzipper-Bot
-cd /Unzipper-Bot
+cd /Unzipper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 bash start.sh
